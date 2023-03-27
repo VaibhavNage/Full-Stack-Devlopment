d9aa8641b5e9f1e1e995785321a5bd3f09d7c3d8
VaibhavNage <vaibhavnage158@gmail.com>
2023-03-27 16:12:15 +0530

    java8

--- src/java8/Vehicle.java
+++ src/java8/Vehicle.java
@@ -10,24 +10,40 @@ public interface Vehicle {
     default void turnAlarmOff(){
         System.out.println("AlarmOff");
     };
+    default void getSpeed(){
+        System.out.println(getBrand());
+        System.out.println(speedUp());
+        System.out.println(slowDown());
+    }
+    static void calculateSpeed(){
+        int speed = 90;
+        if (speed < 20)
+            System.out.println("Low Speed");
+        else
+            System.out.println("High Speed");
+    }
 }
 class Car implements Vehicle{
     public static void main(String[] args) {
-
+        Car car =new Car();
+        car.turnAlarmOn();
+        car.turnAlarmOff();
+        car.getSpeed();
+        Vehicle.calculateSpeed();
     }
 
     @Override
     public String getBrand() {
-        return null;
+        return "Brand Name : Royal Enfield";
     }
 
     @Override
     public String speedUp() {
-        return null;
+        return "Speed Increasing";
     }
 
     @Override
     public String slowDown() {
-        return null;
+        return "Speed Reducing";
     }
 }
